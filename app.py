6c2379b6a310bd8baa761966aef6e4084591bce0
Nicolas Guerrero <saynaysurf@gmail.com>
2020-01-24 12:24:53 -0800

    Got to displaying board

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
 from boggle import Boggle
+from flask import Flask, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "secret"
+debug = DebugToolbarExtension(app)
 
 boggle_game = Boggle()
+
+@app.route("/")
+def index():
+    """ """
+
+    board_list = boggle_game.make_board()
+
+    return render_template("board.html", board_list=board_list)
